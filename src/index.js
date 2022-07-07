25579733a9f073f96bc1d594d0f7b2adfd70c2d8
Alexander Voronenko <796546@gmail.com>
2022-07-07 11:49:32 +0300

    feat:add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    let result = "";
+    let digit1 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let digit2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let numString = number.toString().split("");
+
+    if (number === 0) return "zero";
+    if (number <= 19) { result = digit1[number]; }
+
+    else {
+        if (numString.length < 3) {
+            result = (numString[1] === "0") ? digit2[numString[0] - 2] : digit2[numString[0] - 2] + " " + digit1[numString[1]];
+        } else {
+            result = digit1[numString[0]] + " hundred";
+            if (numString[2] === "0" && numString[1] === "0") return result;
+            if (numString[2] === "0" && numString[1] !== "1") return result += " " + digit2[numString[1] - 2];
+            if (numString[1] + numString[2] < 10) return result += " " + digit1[numString[2]];
+            if (numString[1] + numString[2] <= 19) {
+                return result += " " + digit1[numString[1] + numString[2]];
+            } else {
+                result += " " + digit2[numString[1] - 2];
+                result += " " + digit1[numString[2]];
+                return result;
+            }
+        }
+    }
+    return result;
+
 }
